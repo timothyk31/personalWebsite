c053890995eab33ebf9db4036374642afd898b22
timothyk31 <timothykurniawan16@gmail.com>
2024-12-15 20:55:46 -0600

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      tim
+    </div>
+  );
+}
+
+export default App;
